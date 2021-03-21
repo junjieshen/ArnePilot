c8c2f98e81ded5bd60a4af566379b728722827b0
Arne Schwarck <arneschwarck@gmail.com>
2021-03-21 19:09:57 +0100

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -567,7 +567,7 @@ class Controls:
     controlsState.startMonoTime = int(start_time * 1e9)
     controlsState.forceDecel = bool(force_decel)
     controlsState.canErrorCounter = self.can_error_counter
-    controlsState.decelForModel = self.sm['longitudinalPlan'].longitudinalPlanSource == LongitudinalPlanSource.modelV2
+    controlsState.decelForModel = self.sm['longitudinalPlan'].longitudinalPlanSource == LongitudinalPlanSource.model
     controlsState.mapValid = self.sm['longitudinalPlan'].mapValid
     controlsState.vCurvature = self.sm['longitudinalPlan'].vCurvature
 
